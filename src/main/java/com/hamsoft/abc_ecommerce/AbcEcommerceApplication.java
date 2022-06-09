d3d52af5d63f8728c187a9e67511f266f703d263
ahmedopeyemikabiru <opeyemi.kabiru@yahoo.com>
2022-06-09 18:43:29 +0100

    first commit

---
+++ src/main/java/com/hamsoft/abc_ecommerce/AbcEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.hamsoft.abc_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbcEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hamsoft/abc_ecommerce/AbcEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamsoft.abc_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
