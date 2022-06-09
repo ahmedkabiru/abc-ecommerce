0e6a385437849821b3852160e36bb0dd26bb99e8
ahmedopeyemikabiru <opeyemi.kabiru@yahoo.com>
2022-06-09 19:53:15 +0100

    Added ApiResponse

---
+++ src/main/java/com/hamsoft/abc_ecommerce/config/ApiResponse.java
@@ -0,0 +1,18 @@
+package com.hamsoft.abc_ecommerce.config;
+
+public class ApiResponse {
+    public   final  boolean success;
+    public final  String message;
+
+    public ApiResponse(boolean success, String message) {
+        this.success = success;
+        this.message = message;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+    public String getMessage() {
+        return message;
+    }
+}
